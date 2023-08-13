abda261f120881e92fa04f4353b66dc5164fb10c
AlexanderOI <vialexoriba1257@gmail.com>
2023-08-12 21:11:28 -0400

    Added functionality to sort by price ascending and descending

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components'
 import { sectionProducts } from './json/products.ts'
 import { RenderProducts } from './components/Products'
-import { Products, ProductsList } from './types'
-import { useState } from 'react'
+import { ProductsList } from './types'
+import { ChangeEvent, useState } from 'react'
 
 function App() {
 
@@ -24,27 +24,48 @@ function App() {
   padding: 20px;
   right: 0;
   `
+
+  const DivFilters = styled.div`
+  display: flex;
+  width: 100%;
+  height: 50px;
+  justify-content: space-between;
+  align-items:center;
+  padding-left: 50px;
+  padding-right: 50px;
+
+  `
+
   const [filters, setFilters] = useState({
     category: 'all',
-    minPrice: 20000
+    orderPrice: "mayor"
   })
 
-  const filterProducts = (products: ProductsList[]): ProductsList[] => {
-    return products.filter(product => {
-      return (
-        product.price < filters.minPrice &&
-        (
-          filters.category === 'all' ||
-          product.category === filters.category
-        )
+  const [seclectValue, setSeclectValue] = useState<string>()
 
-      )
+  const filterProducts = (products: ProductsList[]): ProductsList[] => {
+    return products.slice().sort((a, b) => {
+      if (filters.orderPrice === "mayor") {
+        return b.price - a.price
+      } else {
+        return a.price - b.price
+      }
     })
+
   }
 
-  const products = filterProducts(sectionProducts.Almacén['Aderezos/Condimentos'])
-  console.log(products)
+  const handleChangeSelectFilterPrice = (event: ChangeEvent<HTMLSelectElement>) => {
+    const selectedValue = event.target.value;
+    setSeclectValue(selectedValue);
 
+    if (selectedValue === 'mayor') {
+      setFilters({ category: 'all', orderPrice: 'mayor' });
+    } else {
+      setFilters({ category: 'all', orderPrice: 'menor' });
+    }
+  }
+
+  const productsRender = filterProducts(sectionProducts.Almacén['Aderezos/Condimentos'])
 
   return (
     <>
@@ -54,7 +75,14 @@ function App() {
           <span>Ver carrito</span>
         </DivCarrito>
       </Header>
-      <RenderProducts products={products} />
+      <DivFilters>
+        <input type="text" placeholder='Buscar producto' />
+        <select value={seclectValue} onChange={handleChangeSelectFilterPrice}>
+          <option value="mayor">De mayor a menor</option>
+          <option value="menor">De menor a mayor</option>
+        </select>
+      </DivFilters>
+      <RenderProducts products={productsRender} />
     </>
   )
 }
